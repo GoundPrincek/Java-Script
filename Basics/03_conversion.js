ac816796303dcb24f474275573ac10c93225bea0
Princek <princestudy.7498@gmail.com>
2026-03-08 14:48:32 +0000

    learning

--- Basics/03_conversion.js
+++ Basics/03_conversion.js
@@ -1,25 +1,23 @@
-// let score = "33"; 
-// console.log(typeof score);
-
-// let valueInString = Number(score);
-//  console.log(typeof valueInString);
+let score = "33"; 
+console.log(typeof score);
+let valueInString = Number(score);
+console.log(typeof valueInString);
 
 //"33" => 33
 //"33aabbc" => undifined
 //true <=> 1 ; false <=>0
 
 
-// let loggedIn=35
-
-// let booleanIsLoggedIn=Boolean(loggedIn)
-// console.log(typeof booleanIsLoggedIn);
+let loggedIn=35
+let booleanIsLoggedIn=Boolean(loggedIn)
+console.log(typeof booleanIsLoggedIn);
 
 //""=> false
 //"prince"=> true
 
-// let someNumber = 35;
-// let stringNumber = String(someNumber);
-// // console.log(typeof stringNumber);
+let someNumber = 35;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
 
 
 //  ********* OPERATIONS *********  //
@@ -39,3 +37,13 @@ console.log("5"+4+6);
 console.log(5+4+"6");
 
 //Documentation Link : https://tc39.es/ecma262/
+
+
+console.log(+true);
+
+let num1, num2, num3 
+num1=num2=num3=2+2
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter)
---
+++ Basics/05_datatypes.summary.js
@@ -0,0 +1,56 @@
+// primitive datatypes
+
+// 7 types: String ,Number, Boolean, Null ,Undefined ,Symbol ,Bigint
+
+ const score=55;
+ const scorevalue =100.33
+
+ let useremail;
+
+ const outsideTemp=null;
+ const isloggedIn=false;
+
+
+ const id=Symbol("132");
+ const anotherId=Symbol("132")
+
+ console.log(id===anotherId);
+ 
+
+
+//  const bigNumber = 3.141549256685622n
+
+
+//Refrence Type(Non primitive)
+
+// Array ,Objects ,Functions
+
+// const heros =["Shaktiman", "Naagraj","Donga"];
+// let myObj{
+//     name="prince";
+//     age=20;
+
+// }
+
+
+// const myfunction=functions (){
+//     console.log("Hello World");
+//     }
+
+    console.log(typeof outsideTemp);
+
+
+
+
+
+//**********************************************************************
+
+// Stack(Primitive), Heap (Non-Primitive)
+
+let mycollegeName ="Vidyavardhinis college of Engineering"
+let myclassesName = mycollegeName
+
+myclassesName="Pem school"
+console.log(myclassesName);
+console.log(mycollegeName);
+
