d80a17154b9ae08a927afb2d32c1386f88bd005c
Princek <princestudy.7498@gmail.com>
2026-03-15 19:58:58 +0000

    learning strings some topics

--- Basics/06_String.js
+++ Basics/06_String.js
@@ -4,3 +4,18 @@ const repoCount= 5;
 console.log(name + repoCount +" value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const gameName = new String("Princek");
+
+ console.log(gameName[3]);
+ console.log(gameName.__proto__); //****when we write this in browser console then 
+ // this shows the all types of string and there respective solutions
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(5));
+
+ console.log(gameName.indexOf("k"));
+ 
+ 
+ 
+ 
