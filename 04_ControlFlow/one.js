5783e03b38687293dc065b05bd8f6615fce2dac1
Princek <princestudy.7498@gmail.com>
2026-05-05 18:10:00 +0000

    new start

---
+++ 04_ControlFlow/one.js
@@ -0,0 +1,38 @@
+// if 
+
+
+
+// if(5==="4"){
+//     console.log("5 is equal to 4");
+// }
+// else{
+//     console.log("This will not be printed");
+// }
+
+// < , > , <= , >= , == , != ,===
+
+// ********* THis is the important  uncomment this part and check ***************** //
+
+// const score = 200
+
+// if(score > 100 ) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }else{
+//     console.log("there is no power in the user");
+    
+// }
+//  console.log(`user power: ${power}`);
+
+
+
+//******************************************//
+
+const balance =20000
+
+// if (balance >= 20000) console.log("Mai Ameer Hu");
+
+
+
+
