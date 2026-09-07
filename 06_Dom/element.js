ba4e1d9b0110516dd9e096b0f4a2168b4c6e6f14
Prince Gound <Princestudy.7498@gmail.com>
2026-09-07 23:14:23 +0530

    the element in dom learned today

---
+++ 06_Dom/element.js
@@ -0,0 +1,9 @@
+const div = document.createElement("div");
+div.classList.add("card");
+
+const heading = document.createElement("h2");
+// Fix: Add text content so it is visible
+heading.textContent = "JavaScript DOM Revision"; 
+
+div.append(heading);
+document.body.append(div);
