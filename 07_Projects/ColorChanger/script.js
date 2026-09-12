32131f39a7109a8f8ca8fffa38f02b52fb1a5ed8
Prince Gound <Princestudy.7498@gmail.com>
2026-09-13 00:00:34 +0530

    Implement color change on button click and clean up code

--- 07_Projects/ColorChanger/script.js
+++ 07_Projects/ColorChanger/script.js
@@ -2,5 +2,13 @@
 const colorChangerbtn = document.getElementById("btn")
 
 colorChangerbtn.addEventListener("click",function(){
-    console.log("color is changed")
+    const randomcolor = Math.floor(Math.random()* 1677726)
+    .toString(16)
+    .padStart(6,"0")
+    
+      document.body.style.backgroundColor=`#${randomcolor}`
 })
+
+// const BackgroundColor = document/getElementById("body")
+
+// BackgroundColor.body.style.BackgroundColor ="aliceblue"
